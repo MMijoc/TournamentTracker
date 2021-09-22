ab4171a207362983465426e90661df048ee2c2ba
MMijoc <markomijoc18@gmail.com>
2021-09-22 15:36:54 +0200

    Added basic library for the Tournament Tracker

---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+	/// <summary>
+	/// Represents one math in the tournament
+	/// </summary>
+	public class MatchupModel
+	{
+		/// <summary>
+		/// The set of teams that were involved in this math
+		/// </summary>
+		public List<MatchupEntryModel> Entries { get; set; } = new();
+		/// <summary>
+		/// The winner of the match
+		/// </summary>
+		public TeamModel Winner { get; set; }
+		/// <summary>
+		/// Which round this math is a part of
+		/// </summary>
+		public int MatchupRound { get; set; }
+	}
+}
